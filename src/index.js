1acff886d09be929c9702bb9db823017520a7844
Travis Stanley <travisstanley@Admins-MacBook-Air.local>
2017-10-19 00:18:03 -0700

    first completed component of the new module: Tooltip

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Tooltip from './components/tooltip/Tooltip';
+
+module.exports = {
+  Tooltip,
+}
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const BUILD_DIR = path.resolve(__dirname, 'lib');
+const APP_DIR = path.resolve(__dirname, 'src');
+
+const WebpackConfig = {
+  entry: `${APP_DIR}/index.js`,
+
+  output: {
+    path: BUILD_DIR,
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    library: 'Tool-Box',
+  },
+
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /.js$/,
+        exclude: /node_modules/,
+        include: APP_DIR,
+        options: {
+          presets: ['env', 'react', 'stage-0'],
+        },
+      },
+      {
+        test: /\.css$/,
+        include: APP_DIR,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader'],
+        }),
+      },
+    ],
+  },
+
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+};
+
+
+// webpack production config.
+if (process.env.NODE_ENV === 'production') {
+  WebpackConfig.externals = {
+    react: 'react',
+    'react-dom': 'react-dom',
+  };
+
+  WebpackConfig.plugins = [
+    ...WebpackConfig.plugins,
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+      },
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+      },
+      comments: false,
+    }),
+  ];
+}
+
+
+module.exports = WebpackConfig;
