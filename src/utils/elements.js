25c016ee711990b200e6ab20a78c9314e8779268
Travis Stanley <travisstanley@Admins-MacBook-Air.local>
2017-10-20 19:33:16 -0700

    add a few more common elements to the elements file

--- src/utils/elements.js
+++ src/utils/elements.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 const e = React.createElement;
 const clone = React.cloneElement;
 const element = (type) => (props, ...children) => e(type, props, ...children);
-const elements = ['div', 'h1', 'ul', 'ol', 'form', 'p', 'a']
-  .reduce((acc, elem) => (Object.assign(acc, { [elem]: element(elem) })), {});
+const elements = [
+  'div', 'ul', 'ol', 'li', 'select', 'form', 'p', 'a', 'input', 'button', 
+  'span', 'h1', 'h2', 'h3', 'label', 'li', 'select', 'option', 'table', 
+  'thead', 'tbody', 'tfoot', 'tr', 'th', 'td', 'strong', 'em', 'i'
+].reduce((acc, elem) => (Object.assign(acc, { [elem]: element(elem) })), {});
 
 module.exports = Object.assign(
   {},
