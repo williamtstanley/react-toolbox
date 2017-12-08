86f61304e15d2cc4387feca5be7acc793510d5ba
Travis Stanley <travisstanley@Admins-MacBook-Air.local>
2017-12-08 07:48:49 -0800

    more complete list of elements for React Hyperscript implementation

--- src/utils/elements.js
+++ src/utils/elements.js
@@ -4,10 +4,26 @@ import { merge } from './helpers';
 const e = React.createElement;
 const clone = React.cloneElement;
 const element = (type) => (props, ...children) => e(type, props, ...children);
+
 const elements = [
-  'div', 'ul', 'ol', 'li', 'select', 'form', 'p', 'a', 'input', 'button',
-  'span', 'h1', 'h2', 'h3', 'label', 'li', 'select', 'option', 'table',
-  'thead', 'tbody', 'tfoot', 'tr', 'th', 'td', 'strong', 'em', 'i', 'svg', 'use'
+  'a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article', 'aside',
+  'audio', 'b', 'base', 'basefont', 'bdi', 'bdo', 'bgsound', 'big', 'blink',
+  'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'cite',
+  'code', 'col', 'colgroup', 'command', 'content', 'data', 'datalist', 'dd',
+  'del', 'details', 'dfn', 'dialog', 'dir', 'div', 'dl', 'dt', 'element', 'em',
+  'embed', 'fieldset', 'figcaption', 'figure', 'font', 'footer', 'form',
+  'frame', 'frameset', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header',
+  'hgroup', 'hr', 'html', 'i', 'iframe', 'image', 'img', 'input', 'ins',
+  'isindex', 'kbd', 'keygen', 'label', 'legend', 'li', 'link', 'listing',
+  'main', 'map', 'mark', 'marquee', 'math', 'menu', 'menuitem', 'meta',
+  'meter', 'multicol', 'nav', 'nextid', 'nobr', 'noembed', 'noframes',
+  'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param',
+  'picture', 'plaintext', 'pre', 'progress', 'q', 'rb', 'rbc', 'rp', 'rt',
+  'rtc', 'ruby', 's', 'samp', 'script', 'section', 'select', 'shadow', 'slot',
+  'small', 'source', 'spacer', 'span', 'strike', 'strong', 'style', 'sub',
+  'summary', 'sup', 'svg', 'table', 'tbody', 'td', 'template', 'textarea',
+  'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'tt', 'u', 'ul',
+  'var', 'video', 'wbr', 'xmp',
 ].reduce((acc, elem) => (merge(acc, { [elem]: element(elem) })), {});
 
 module.exports = merge({
